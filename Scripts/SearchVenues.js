2a1aec6aa707527693b88700e63c4383b3bc2c15
Shariq <imamshariq08@gmail.com>
2017-02-05 21:57:07 +0000

    Js & style updated
    Global variables created  also styles updated

--- Scripts/SearchVenues.js
+++ Scripts/SearchVenues.js
@@ -1,1 +1,8 @@
-﻿
+﻿// Global Variables
+var _MainClass = null;
+var _FourSquareApi = null;
+
+$(document).ready(function () {
+    _MainClass = new MainClass();
+    _MainClass.InitializeMainClass();
+});
