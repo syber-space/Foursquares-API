f1453ae523213b09a162b12923ebad5b7129fb3c
Shariq <imamshariq08@gmail.com>
2017-02-05 21:31:34 +0000

    Revert "Style and js updated"
    This reverts commit bee2334c8b44b0fe4ed787ae8a81305a2adf017b.

--- Scripts/SearchVenues.js
+++ Scripts/SearchVenues.js
@@ -1,8 +1,1 @@
-﻿// Global Variables
-var _MainClass = null;
-var _FourSquareApi = null;
-
-$(document).ready(function () {
-    _MainClass = new MainClass();
-    _MainClass.InitializeMainClass();
-});
+﻿
